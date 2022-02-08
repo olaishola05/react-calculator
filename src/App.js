b6472f0f9ed7de9316701bd0509e11f1c416e8f7
Ola Ishola <45001916+netman5@users.noreply.github.com>
2022-02-08 18:43:04 +0100

    Deleted boilerplates and removed dead live link as requested

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from 'react';
 import './App.css';
 import Calculator from './Components/Calculator';
 
-function App() {
-  return (
-    <div className="App">
-      <Calculator />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className="App">
+          <Calculator />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,5 +9,3 @@ ReactDOM.render(
   // eslint-disable-next-line comma-dangle
   document.getElementById('root')
 );
-
-reportWebVitals();
